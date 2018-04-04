ec7cf040e11d908213a027adf2db397d7c36026d
Miguel A. Angulo <maangulo12@gmail.com>
2018-04-04 01:49:25 -0500

    Added config and views modules.

--- backend/__init__.py
+++ backend/__init__.py
@@ -10,8 +10,13 @@
 from flask import Flask
 
 
-app = Flask(__name__)
+# Creating the Flask app
+app = Flask(__name__,
+            template_folder='../frontend',
+            static_folder='../frontend')
+            
+# Configuring the app from config module
+app.config.from_pyfile('config.py')
 
-@app.route("/")
-def hello():
-    return "<h1>ChartBulls Web App</h1>"
+# Importing the app views
+from backend import views
